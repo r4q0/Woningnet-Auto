cd80e2b12588605a111b0bac2959c73ab61a2a7e
r5q0 <bilalemailsss@gmail.com>
2025-01-04 16:15:50 +0100

    Refactor Selenium script to streamline login and tab handling, improving readability and maintainability

--- index.py
+++ index.py
@@ -37,27 +37,21 @@ try:
     except Exception as e:
         print(f"Error clicking accept cookies button: {e}")
 
-    # Wait for the login form to be present
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "LoginForm"))
     )
 
-    # Enter login credentials (replace with your actual credentials)
     username = os.getenv('EMAIL')
     password = os.getenv('PASSWORD')
 
-    # Locate the username and password fields and enter the credentials
     driver.find_element(By.ID, "Input_UsernameVal").send_keys(username)
     driver.find_element(By.ID, "Input_PasswordVal").send_keys(password)
 
-    # Submit the login form
     driver.find_element(By.ID, "b8-Button").click()
 
     time.sleep(random.uniform(2, 5))
-    # Now navigate to the target page
     driver.get(os.getenv('URL') + "/WoningOverzicht")
 
-    # Wait for the expand button to be clickable
     try: 
         expand_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[@data-expression='' and contains(@class, 'font-bold') and text()='Match met je wensen']"))
@@ -66,7 +60,6 @@ try:
     except Exception as e:
         print(f"Error clicking expand button: {e}")
 
-    # Wait for the sort button to be clickable
     try:
         sort_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[@data-expression='' and text()='Tijd over om te reageren']"))
@@ -75,33 +68,28 @@ try:
     except Exception as e:
         print(f"Error clicking sort button: {e}")
 
-    # Find the parent div
     parent_div = driver.find_element(By.XPATH, "//div[@data-list='' and contains(@class, 'list list-group border_width_line_top OSFillParent')]")
-    # Find all clickable divs inside the parent div
     clickable_divs = parent_div.find_elements(By.CSS_SELECTOR, ".OSBlockWidget a")
 
-    # Open each div in a new tab
     for i, div in enumerate(clickable_divs[:int(os.getenv('MAX-INSCHRIJVINGEN'))]):
         href = div.get_attribute("href")
         driver.execute_script(f"window.open('{href}', '_blank');")
-        time.sleep(random.uniform(1, 3))  # Random sleep to mimic human behavior
+        time.sleep(random.uniform(1, 3))
 
         print("opening new tab")
         driver.switch_to.window(driver.window_handles[-1])
 
-        # Wait for the button to be clickable and click it
         try:
             reageren_button = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, "//button[@class='btn btn-primary OSFillParent' and text()='Reageren op deze Woning']"))
             )
-            reageer_button_text = reageren_button.text  # Ensure we get the correct button
+            reageer_button_text = reageren_button.text
             print(f"Clicking button: {reageer_button_text} on tab {i + 1}")
             reageren_button.click()
         except Exception as e:
             print(f"Error clicking reageren button on tab {i + 1}: {e}")
 
-        # After clicking, switch back to the main tab (index 0)
-        time.sleep(random.uniform(1, 3))  # Random sleep before proceeding to the next div
+        time.sleep(random.uniform(1, 3))
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
     
