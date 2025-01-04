eaf59c0bedf2ad5fff39a8e290757008fe89f651
r5q0 <bilalemailsss@gmail.com>
2025-01-04 15:57:27 +0100

    Add MAX-INSCHRIJVINGEN variable and update tab opening logic to use its value

--- index.py
+++ index.py
@@ -80,7 +80,7 @@ parent_div = driver.find_element(By.XPATH, "//div[@data-list='' and contains(@cl
 clickable_divs = parent_div.find_elements(By.CSS_SELECTOR, ".OSBlockWidget a")
 
 # Open each div in a new tab
-for i, div in enumerate(clickable_divs[:2]):
+for i, div in enumerate(clickable_divs[:int(os.getenv('MAX-INSCHRIJVINGEN'))]):
     href = div.get_attribute("href")
     driver.execute_script(f"window.open('{href}', '_blank');")
     time.sleep(random.uniform(1, 3))  # Random sleep to mimic human behavior
